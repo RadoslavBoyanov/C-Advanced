74ee221789700cfcfcf2e19b29a78d9acc0df06f
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-10-19 13:34:55 +0300

    Exercise - simple games

---
+++ ExamPreparation/Simple programs on Gaming/Hangman/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace Hangman
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+            Console.WriteLine("Welcome to Hangman!");
+			string[] listOfWords = new string[10];
+			listOfWords[0] = "sheep";
+			listOfWords[1] = "goat";
+			listOfWords[2] = "computer";
+			listOfWords[3] = "lemon";
+			listOfWords[4] = "watermelon";
+			listOfWords[5] = "icecream";
+			listOfWords[6] = "jasmine";
+			listOfWords[7] = "pineapple";
+			listOfWords[8] = "orange";
+			listOfWords[9] = "mango";
+
+			Random random = new Random();
+
+			var indx = random.Next(listOfWords.Length);
+
+			string mysteryWord = listOfWords[indx];
+
+			char[] guess = new char[mysteryWord.Length];
+
+			for (int i = 0; i < mysteryWord.Length; i++)
+			{
+				guess[i] = '*';
+			}
+
+			int wrongGuesses = 0;
+			int maxAttempts = 6;
+
+			Console.WriteLine("Enter your guess: ");
+
+			while (true)
+			{
+				string input = Console.ReadLine();
+
+				if(string.IsNullOrEmpty(input) || input.Length != 1)
+				{
+					Console.WriteLine("Invalid input. Please enter a single character.");
+					continue;
+				}
+
+				char playerGuess = char.ToLower(input[0]);
+				bool isGuessCorrect = false;
+                for (int j = 0; j < mysteryWord.Length; j++)
+                {
+					if (playerGuess == mysteryWord[j])
+					{
+						guess[j] = playerGuess;
+						isGuessCorrect = true;
+					}
+                }
+
+				if (!isGuessCorrect)
+				{
+					wrongGuesses++;
+                    Console.WriteLine($"Wrong guess! You have {maxAttempts - wrongGuesses} attempts left!");
+                }
+
+				Console.WriteLine(guess);
+
+				if(new string(guess) == mysteryWord)
+				{
+					Console.WriteLine("Congratulations! You guessed the word!");
+					break;
+				}
+
+				if (wrongGuesses >= maxAttempts)
+				{
+                    Console.WriteLine("Game over! You've run out of attempts.");
+                    Console.WriteLine($"The correct word was: {mysteryWord}");
+					break;
+                }
+            }
+        }
+	}
+}
---
+++ ExamPreparation/Simple programs on Gaming/Tower of Hanoi/Program.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Tower_of_Hanoi
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			char startPeg = 'A';
+			char ednPeg = 'C';
+			char tempPeg = 'B';
+			int totalDisks = 3;
+
+			solveTowers(totalDisks, startPeg, ednPeg, tempPeg);
+
+		}
+
+		private static void solveTowers(int n, char startPeg, char ednPeg, char tempPeg)
+		{
+			if (n > 0)
+			{
+				solveTowers(n - 1, startPeg, tempPeg, ednPeg);
+                Console.WriteLine("Move disk from " + startPeg + " " + ednPeg);
+				solveTowers(n - 1, tempPeg, ednPeg, startPeg);
+            }
+		}
+	}
+}
